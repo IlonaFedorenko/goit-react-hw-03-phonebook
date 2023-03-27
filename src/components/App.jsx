648180aba126216c0d22fbed5b36f681ea8a91bc
IlonaFedorenko <86719203+IlonaFedorenko@users.noreply.github.com>
2023-03-27 21:30:17 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ export class App extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.state !== prevState) {
+    if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
